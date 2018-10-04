ae2e54e3eedfea2d1beb103a1556a16a4085c412
charkitch <charles.kitchen@gmail.com>
2018-10-04 15:25:42 -0400

    change stroke styling to improve visibility

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import Game from './lib/game';
-
+import HomeScreen from './lib/home_screen';
 let gameStarted = true;
 const canvas = document.getElementById("game-world");
 const context = canvas.getContext('2d');
@@ -9,4 +9,6 @@ if (gameStarted) {
   const BadSkyCab = new Game(canvas, context);
   BadSkyCab.render();
   window.game = BadSkyCab;
+} else {
+  const HomeScreen = new HomeScreen(canvas, context);
 }
