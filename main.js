aabcec6b5609fb53b8dc563da9a6f15caef8cc41
charkitch <charles.kitchen@gmail.com>
2018-10-04 22:32:03 -0400

    begin adding sountrack

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 import Game from './lib/game';
 import HomeScreen from './lib/home_screen';
+import Sound from './lib/audio/sound';
+
+let theme = new Sound('./assets/audio/317363.mp3');
+theme.play();
+
 let gameStarted = true;
 const canvas = document.getElementById("game-world");
 const context = canvas.getContext('2d');
-
 
 if (gameStarted) {
   const BadSkyCab = new Game(canvas, context);
