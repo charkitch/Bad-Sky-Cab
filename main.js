5bcccb01192d61419806f92ec851d62c83a43cfc
charkitch <charles.kitchen@gmail.com>
2018-10-05 12:19:53 -0400

    implement toggling title

--- main.js
+++ main.js
@@ -1,36 +1,21 @@
 import Game from './lib/game';
-import HomeOverlay from './lib/interface/home_overlay';
 import Sound from './lib/audio/sound';
 
 class Page {
   constructor() {
-    this.theme = new Sound('./assets/audio/317363.mp3');
-    this.gameStarted = false;
+    this.started = false;
     this.canvas = document.getElementById("game-world");
     this.context = this.canvas.getContext('2d');
-    this.homeOverlay = new HomeOverlay(this.canvas, this.context);
     this.game = new Game(this.canvas, this.context);
-    this.keyDownHandler = this.keyDownHandler.bind(this);
+    this.paused = false;
   }
 
   gameStatusToggle() {
     this.gameStarted = ! this.gameStarted;
   }
 
-  keyDownHandler(e) {
-    this.gameStatusToggle();
-  }
-
   render() {
-    if (this.gameStarted) {
-      this.theme.play();
       this.game.render();
-    } else {
-      debugger
-      let glerd = document.addEventListener("keydown", this.keyDownHandler);
-      debugger
-      this.homeOverlay.draw(this.context);
-    }
   }
 
 }
